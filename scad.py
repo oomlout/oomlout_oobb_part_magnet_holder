f18de9e8951d309f56f19c8623861b42422bad45
Oom Lout <aaron@oomlout.com>
2025-09-10 20:55:47 +0100

    generated

--- scad.py
+++ scad.py
@@ -20,8 +20,8 @@ def make_scad(**kwargs):
         typ = "fast"
         #typ = "manual"
 
-    oomp_mode = "project"
-    #oomp_mode = "oobb"
+    #oomp_mode = "project"
+    oomp_mode = "oobb"
 
     test = False
     #test = True
@@ -118,20 +118,34 @@ def make_scad(**kwargs):
         part_default["full_shift"] = [0, 0, 0]
         part_default["full_rotations"] = [0, 0, 0]
         
-        part = copy.deepcopy(part_default)
-        p3 = copy.deepcopy(kwargs)
-        p3["width"] = 3
-        p3["height"] = 3
-        #p3["thickness"] = 6
-        #p3["extra"] = ""
-        part["kwargs"] = p3
-        nam = "base"
-        part["name"] = nam
-        if oomp_mode == "oobb":
-            p3["oomp_size"] = nam
-        if not test:
-            pass
-            #parts.append(part)
+        ps = []
+        
+        p = {}        
+        p["magnet"] = "hardware_magnet_cylinder_6_mm_diameter_6_mm_depth"
+        p["width"] = 3
+        p["height"] = 1
+        p["magnet_shape"] = "cylinder"
+        p["magnet_diameter"] = 6
+        p["magnet_depth"] = 6
+        p["magnet_multiple"] = 3
+        p["magnet_offset"] = 7.5
+        ps.append(p)
+
+
+        for p in ps:
+            part = copy.deepcopy(part_default)
+            p3 = copy.deepcopy(kwargs)
+            p3.update(p)
+            p3["thickness"] = p3["magnet_depth"] + 1.5
+            p3["extra"] = f"{p3["magnet"]}_magnet_{p3["magnet_multiple"]}_multiple"
+            part["kwargs"] = p3
+            nam = f"magnet_holder"
+            part["name"] = nam
+            if oomp_mode == "oobb":
+                p3["oomp_size"] = nam
+            if not test:
+                pass
+                parts.append(part)
 
 
     kwargs["parts"] = parts
@@ -209,7 +223,122 @@ def get_base(thing, **kwargs):
         p3["pos"] = pos1
         #p3["m"] = "#"
         oobb_base.append_full(thing,**p3)
+
+
+def get_magnet_holder(thing, **kwargs):
+    magnet_shape = kwargs.get("magnet_shape", "cylinder")
+    if magnet_shape == "cylinder":
+        return get_magnet_holder_cylinder(thing, **kwargs)
+
+
+def get_magnet_holder_cylinder(thing, **kwargs):
+    prepare_print = kwargs.get("prepare_print", False)
+    width = kwargs.get("width", 1)
+    height = kwargs.get("height", 1)
+    depth = kwargs.get("thickness", 3)                    
+    rot = kwargs.get("rot", [0, 0, 0])
+    pos = kwargs.get("pos", [0, 0, 0])
+    extra = kwargs.get("extra", "")
     
+    #magnet
+    magnet_diameter = kwargs.get("magnet_diameter", 6)
+    magnet_depth = kwargs.get("magnet_depth", 6)
+    magnet_multiple = kwargs.get("magnet_multiple", 1)
+    magnet_offset = kwargs.get("magnet_offset", 7.5)
+
+    #add plate
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "positive"
+    p3["shape"] = f"oobb_plate"    
+    p3["depth"] = depth
+    #p3["holes"] = True         uncomment to include default holes
+    #p3["m"] = "#"
+    pos1 = copy.deepcopy(pos)         
+    p3["pos"] = pos1
+    oobb_base.append_full(thing,**p3)
+    
+    #add holes seperate
+    p3 = copy.deepcopy(kwargs)
+    p3["type"] = "p"
+    p3["shape"] = f"oobb_holes"
+    p3["radius_name"] = "m3"
+    p3["both_holes"] = True  
+    p3["depth"] = depth
+    p3["holes"] = ["top","bottom"]
+    #p3["m"] = "#"
+    pos1 = copy.deepcopy(pos)         
+    p3["pos"] = pos1
+    oobb_base.append_full(thing,**p3)
+
+    #add magnet holes
+    if True:
+        extra = 0.5
+        magnet_diameter = kwargs.get("magnet_diameter", 6)
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_cylinder"
+        dep = magnet_depth + extra
+        p3["depth"] = dep
+        p3["radius"] = (magnet_diameter + extra)/2
+        for i in range(magnet_multiple+1):
+            p4 = copy.deepcopy(p3)
+            pos1 = copy.deepcopy(pos)         
+            pos1[0] += (i - (magnet_multiple-1)/2) * magnet_offset
+            pos1[2] += dep /2
+            p3["pos"] = pos1
+            #p3["m"] = "#"
+            oobb_base.append_full(thing,**p4)
+
+    #add counter sunk screws on side
+    if True:    
+        counter_sunk_extra = 0.5
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_screw_countersunk"
+        p3["depth"] = depth - counter_sunk_extra
+        p3["radius_name"] = "m3"
+        p3["clearance"] = "top"
+        pos1 = copy.deepcopy(pos)
+        pos1[2] += depth - counter_sunk_extra
+        poss = []
+        pos11 = copy.deepcopy(pos1)
+        pos11[0] += (width-1)/2 * 15
+        poss.append(pos11)
+        pos12 = copy.deepcopy(pos1)
+        pos12[0] += -(width-1)/2 * 15
+        poss.append(pos12)
+        p3["pos"] = poss
+        #p3["m"] = "#"
+        oobb_base.append_full(thing,**p3)
+
+
+    if prepare_print:
+        #put into a rotation object
+        components_second = copy.deepcopy(thing["components"])
+        return_value_2 = {}
+        return_value_2["type"]  = "rotation"
+        return_value_2["typetype"]  = "p"
+        pos1 = copy.deepcopy(pos)
+        pos1[0] += 50
+        return_value_2["pos"] = pos1
+        return_value_2["rot"] = [180,0,0]
+        return_value_2["objects"] = components_second
+        
+        thing["components"].append(return_value_2)
+
+    
+        #add slice # top
+        p3 = copy.deepcopy(kwargs)
+        p3["type"] = "n"
+        p3["shape"] = f"oobb_slice"
+        pos1 = copy.deepcopy(pos)
+        pos1[0] += -500/2
+        pos1[1] += 0
+        pos1[2] += -500/2        
+        p3["pos"] = pos1
+        #p3["m"] = "#"
+        oobb_base.append_full(thing,**p3)
+
 if __name__ == '__main__':
     kwargs = {}
     main(**kwargs)
